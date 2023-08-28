bf6085b08802255f05d6dac96f14cd9dae2235fa
Guilherme Guimarães <116769796+GlGuimaraes@users.noreply.github.com>
2023-08-27 21:51:48 -0300

    Melhorando o código

--- desafio.kt
+++ desafio.kt
@@ -8,15 +8,15 @@ enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
 class Usuario(val nome: String, val idade: Int, val sexo: String)
 
-data class ConteudoEducacional(val nome: String, val materia: String, val duracao: Int = 60,)
+data class ConteudoEducacional(val nome: String, val materia: String, val duracao: Int = 60)
 
 data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>, val nivel: Nivel) {
 
-    val inscritos = mutableListOf<Usuario>()
+    private val inscritos = mutableListOf<Usuario>()
 
     fun matricular(vararg usuarios: Usuario1) = usuarios.forEach { inscritos.add(it) }
-    fun mostrarConteudos() = conteudos.forEach { println(it.nome ) }
-    fun AlunosInscritos() = inscritos.forEach { println(it.nome) }
+    fun mostrarConteudos() = conteudos.forEach { println("${it.nome}, com duração de ${it.duracao} Horas")}
+    fun alunosInscritos() = inscritos.forEach { println("${it.nome} ${it.idade} anos de idade e do sexo: ${it.sexo}") }
 
 }
 
@@ -24,35 +24,35 @@ data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>, 
 
 fun main() {
 
-    val aluno_Rafael = Usuario("Rafael", 18, "M")
-    val aluna_Maria = Usuario("Maria", 32, "F")
-    val aluno_Paulo = Usuario("Paulo", 30, "M")
-    val aluno_Lucas = Usuario("Lucas", 19, "M")
-    val aluna_Rafaela = Usuario("Rafaela", 23, "F")
-    val aluna_Laura = Usuario("Laura", 18, "F")
+    val alunoRafael = Usuario("Rafael", 18, "M")
+    val alunaMaria = Usuario("Maria", 32, "F")
+    val alunoPaulo = Usuario("Paulo", 30, "M")
+    val alunoLucas = Usuario("Lucas", 19, "M")
+    val alunaRafaela = Usuario("Rafaela", 23, "F")
+    val alunaLaura = Usuario("Laura", 18, "F")
 
-    val ConteudosGit: List<ConteudoEducacional> = listOf(
+    val conteudosGit: List<ConteudoEducacional> = listOf(
         ConteudoEducacional("Git e Github", "git"),
         ConteudoEducacional("Github do 0 ao avançado", "git")
     )
-    val ConteudosKotlin: List<ConteudoEducacional> = listOf(
+    val conteudosKotlin: List<ConteudoEducacional> = listOf(
         ConteudoEducacional("A história do Kotlin", "Kotlin"),
         ConteudoEducacional("Kotlin Orientado a Obejos", "Kotlin"),
         ConteudoEducacional("Kotlin Do 0 ao Avançado", "Kotlin")
     )
-    val ConteudosAndroidStudio: List<ConteudoEducacional> = listOf(
+    val conteudosAndroidStudio: List<ConteudoEducacional> = listOf(
         ConteudoEducacional("A história do Android", "AndroidStudio"),
         ConteudoEducacional("TextView e Button", "AndroidStudio"),
         ConteudoEducacional("ImageView e LinearLayout", "AndroidStudio")
     )
 
-    val Git = Formacao("Turma Formação Git", ConteudosGit,Nivel.INTERMEDIARIO)
-    val Kotlin = Formacao("Turma Kotlin", ConteudosKotlin, Nivel.BASICO)
-    val AndroidStudio = Formacao("Turma Android", ConteudosAndroidStudio, Nivel.DIFICIL)
+    val git = Formacao("Turma Formação Git", conteudosGit,Nivel.INTERMEDIARIO)
+    val kotlin = Formacao("Turma Kotlin", conteudosKotlin, Nivel.BASICO)
+    val androidStudio = Formacao("Turma Android", conteudosAndroidStudio, Nivel.DIFICIL)
 
-    ConteudosGit?.let {Git.matricular(aluna_Maria, aluno_Rafael)}
-    ConteudosKotlin?.let {Kotlin.matricular(aluno_Paulo, aluna_Laura) }
-    ConteudosAndroidStudio?.let { AndroidStudio.matricular(aluno_Lucas, aluna_Rafaela,aluna_Laura)}
+    conteudosGit.let {git.matricular(alunaMaria, alunoRafael)}
+    val let = conteudosKotlin.let { kotlin.matricular(alunoPaulo, alunaLaura) }
+    conteudosAndroidStudio.let { androidStudio.matricular(alunoLucas, alunaRafaela,alunaLaura)}
 
 
 
@@ -61,27 +61,30 @@ fun main() {
     println("[2] - Turma Kotlin")
     println("[3] - Turma Android")
 
-    val resposta = readln()
-
-    if (resposta == "1"){
-        println("Bem vindo a Formação: ${Git.nome}")
-        println("Conteudos:")
-        Git.mostrarConteudos()
-        println("Aluno Matriculados:")
-        Git.AlunosInscritos()
-    } else if(resposta == "2"){
-        println("Bem vindo a Formação: ${Kotlin.nome}")
-        println("Conteudos:")
-        Kotlin.mostrarConteudos()
-        println("Alunos Matriculados:")
-        Kotlin.AlunosInscritos()
-    }else if (resposta == "3"){
-        println("Bem vindo a Formação: ${AndroidStudio.nome}")
-        println("Conteudos:")
-        AndroidStudio.mostrarConteudos()
-        println("Alunos Matriculados:")
-        AndroidStudio.AlunosInscritos()
-    } else{
-        println("Digite uma opção válida")
+    when (readln()) {
+        "1" -> {
+            println("Bem vindo a Formação ${git.nome}")
+            println("Conteudos:")
+            git.mostrarConteudos()
+            println("Alunos Matriculados:")
+            git.alunosInscritos()
+        }
+        "2" -> {
+            println("Bem vindo a Formação: ${kotlin.nome}")
+            println("Conteudos:")
+            kotlin.mostrarConteudos()
+            println("Alunos Matriculados:")
+            kotlin.alunosInscritos()
+        }
+        "3" -> {
+            println("Bem vindo a Formação: ${androidStudio.nome}")
+            println("Conteudos:")
+            androidStudio.mostrarConteudos()
+            println("Alunos Matriculados:")
+            androidStudio.alunosInscritos()
+        }
+        else -> {
+            println("Digite uma opção válida")
+        }
     }
 }
